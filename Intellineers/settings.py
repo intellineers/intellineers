8aca3eab0a40c5527b174c2fc8df604696923a3c
cwittlinger <c.wittlinger@c-quadrat.de>
2015-12-15 17:48:42 +0100

    Changed static locations

--- Intellineers/settings.py
+++ Intellineers/settings.py
@@ -132,7 +132,6 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
