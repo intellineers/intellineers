5534f9b1750008d015d4372d700789aeac4a4c47
cs-intellineers <c.sinhart@intellineers.com>
2016-04-05 15:12:55 +0200

    Changed the url.py of the website so that the intermediate view is not shown.

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 import views
 
 urlpatterns = [
-    url(r'^$', views.intermediateView, name="intermediateView"),
-    url(r'^base$', views.basetemplate, name="basetemplate"),
+    # url(r'^$', views.intermediateView, name="intermediateView"),
+    # url(r'^base$', views.basetemplate, name="basetemplate"),
+    url(r'[.]*', views.basetemplate, name="basetemplate"),
 ]
